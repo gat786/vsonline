9ce00763bda11e6ab9d172db90d93bf4eab55dc0
ganesh tiwari <gat786@studentpartner.com>
2019-11-16 17:21:10 +0000

    Co-authored-by: Salman Muin Kayser Chishti <32169182+salmanmkc@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace workspace
+{
+    class Program
+    {
+        // this is the Main method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
+            Console.WriteLine(addition(5, 5));
+            
+            var t1 = new Task(() =>
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.WriteLine($"Printing from the thread {i}");
+                }
+            });
+            
+            for(int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"Printinf from the Main thread");
+
+            }
+            t1.Start();
+
+
+
+        }
+
+        public static int addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        
+
+
+    }
+}
